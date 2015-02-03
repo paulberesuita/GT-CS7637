0704e9dad31361fa85fe69ddf2e054d39d0910a8
Paul Beresuita <paul.beresuita@gmail.com>
2015-02-02 22:17:34 -0500

    Initial Commit

---
+++ src/project1/FrameC.java
@@ -0,0 +1,22 @@
+package project1;
+
+import java.util.ArrayList;
+
+public class FrameC {
+	
+	ArrayList<RavensObject> objects = null;
+	
+	public FrameC(ArrayList<RavensObject> objects) {
+		
+		this.objects = objects;
+	}
+
+	public ArrayList<RavensObject> getObjects() {
+		return objects;
+	}
+
+	public void setObjects(ArrayList<RavensObject> objects) {
+		this.objects = objects;
+	}
+
+}
