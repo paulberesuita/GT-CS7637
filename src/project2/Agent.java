71b1e40ca537f3f6ea00fd4ef85ec29dd7469cfb
Paul Beresuita <paul.beresuita@gmail.com>
2015-02-10 23:21:03 -0500

    Testing commit

--- src/project2/Agent.java
+++ src/project2/Agent.java
@@ -58,7 +58,7 @@ public class Agent {
 
         String solution = "1";
 
-        //I am in the Phase 1 branch
+        //I am testing commit
         //Check which algorithm to use
         if(problem.getProblemType().toString().equals("2x1") || problem.getProblemType().toString().equals("2x2")) {
 
